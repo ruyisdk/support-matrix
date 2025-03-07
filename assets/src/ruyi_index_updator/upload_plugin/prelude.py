05424b8caf5c36f3df6e83b8f5fbb9eb06fe41e8
Ling Wang <lingwang@wcysite.com>
2025-03-07 15:59:15 +0800

    Updator: Remove old stuff

--- assets/src/ruyi_index_updator/upload_plugin/prelude.py
+++ assets/src/ruyi_index_updator/upload_plugin/prelude.py
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 
 try:
     from src.ruyi_index_updator.ruyi_index_parser import *
-    from src.matrix_parser.version import VInfo
     from src.matrix_parser import SystemIdentifier, SystemInfo
     from src.ruyi_index_updator.upload_plugin_base import UploadPluginBase
 except ModuleNotFoundError:
